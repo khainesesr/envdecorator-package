4324657d8a0e4cbb95da1580a1b105c37741b924
khainesesr <102272560+khainesesr@users.noreply.github.com>
2024-10-04 15:01:43 +1300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='envdecorator',
     version='0.1',
-    packages=find_packages(),
-    install_requires=['os','dotenv','functools','python-dotenv'],
-    description='A simple Python package that includes a logging decorator.',
-    author='Kaitlin',
+    py_modules=['envdecorator'],
+    description='A simple Python decorator to install env files',
+    author='Kaitlin Haines',
     author_email='kaitlin.haines@esr.cri.nz',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
+    install_requires=['os','dotenv','functools','python-dotenv'],
 )
